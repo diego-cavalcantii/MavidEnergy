4c5840c8744df0debcd0dd5a31ab2cba72998937
Diego <diego.scavalcanti@gmail.com>
2024-11-22 01:39:34 -0300

    feat put

--- src/main/java/br/com/mavidenergy/gateways/controllers/EnderecoController.java
+++ src/main/java/br/com/mavidenergy/gateways/controllers/EnderecoController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping("/enderecos")
